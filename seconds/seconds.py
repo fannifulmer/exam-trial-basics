905855803a2c0587d2e1e63dea9cc455d4d2b92b
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-10 10:58:11 +0200

    It prints one number right! Progress!

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -2,18 +2,13 @@
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
-def seconds(list_of_numbers = []):
-    for i in range(len(list_of_numbers)+1):
-        if list_of_numbers[i] % 2 == 0:
-            list_of_numbers.remove(list_of_numbers[i])
-            i += 1
-            
-print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
 
-'''list_of_numbers = [1, 2, 3, 4, 5]
-numbers = []
-for i in range(len(list_of_numbers)):
-if list_of_numbers[i] % 2 == 0:
-    numbers.append(list_of_numbers[i])
-    i += 1
-print(numbers)'''
+def seconds(list_of_numbers = []):
+    new_list = []
+    for i in range(len(list_of_numbers)):
+        if list_of_numbers[i] % 2 == 0:
+            new_list.append(list_of_numbers[i])
+            i += 1
+            return new_list
+            #list_of_numbers = new_list
+print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
