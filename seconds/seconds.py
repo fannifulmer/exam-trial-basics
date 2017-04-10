23ba1487ffb7846275a53fbbe6d17b0f7a6a0988
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-10 10:35:17 +0200

    got the idea without a function, trying to put it in the funcion

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,21 @@
-def seconds(change_this):
-    pass
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
+def seconds(list_of_numbers = []):
+    numbers = []
+    for i in range(list_of_numbers):
+        numbers.append(list_of_numbers[i])
+        i += 1
+            
+    
+
 print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
+
+'''list_of_numbers = [1, 2, 3, 4, 5]
+numbers = []
+for i in range(len(list_of_numbers)):
+if list_of_numbers[i] % 2 == 0:
+    numbers.append(list_of_numbers[i])
+    i += 1
+print(numbers)'''
