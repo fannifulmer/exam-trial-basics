544ce7354f4d03fef6b7708b08de4aafb4015040
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-10 09:48:53 +0200

    started working on count-as, not finished yet

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,8 +1,14 @@
-def count_as(change_this):
-    pass
-    # Create a function that takes a filename as string parameter,
-    # counts the occurances of the letter "a", and returns it as a number.
-    # If the file does not exist, the function should return 0 and not break.
-
+def count_as(a_counter):
+    count_as = open('afile.txt', 'r')
+    a_counter = count_as.read()
+#try:
+    dic = dict()
+    for i in count_as:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+    return dic
+#except:
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
