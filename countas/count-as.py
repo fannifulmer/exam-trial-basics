df2bb3b51fc2c5398b29a842e95eb9a7ee9e03d5
Fanni Fulmer <fannifulmer@gmail.com>
2017-04-10 12:07:52 +0200

    tried it without loops, it counts it twice

--- countas/count-as.py
+++ countas/count-as.py
@@ -1,16 +1,13 @@
-def count_as(count_as = open('afile.txt', 'r')):
-    count_as = open('afile.txt', 'r')
-    a_counter = count_as.read()
-#try:
-counter = 0
-for i in range(len(count_as)):
-    if count_as[i] == 'a':
-        counter += 1
-return counter
-
+def count_as(count_as):
+    try:
+        count_as = open('afile.txt', 'r')
+        a_counter = count_as.readline()
+        a_counter = a_counter.lower()
+        return a_counter.count('a')
+    except FileNotFoundError:
+        print("0")
 print(count_as("afile.txt")) # should print 28
 print(count_as("not-a-file")) # should print 0
 
-#except FileNotFoundError:
-    #print("0")
+
 
